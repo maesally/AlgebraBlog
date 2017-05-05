1c5f87b2322fce1eb22c10d40fd9ad68e5847e8c
programeri <programeri@ucione.local>
2017-05-05 20:16:30 +0200

    Dodan gumb za brisanje u itemu

--- resources/views/items/show.blade.php
+++ resources/views/items/show.blade.php
@@ -4,9 +4,19 @@
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
+		
 		<h2>{{ $item->title }}</h2>
 		<h3>{{ $item->created_at }}</h3>
 		<div>{{ $item->Content }}</div>
+		
+		<!-- Provjera da li je user vlasnik itema -->
+		
+		@if ( auth()->id() == $item->user_id )
+		
+		<br><a href="{{ url('/item/'.$item->id.'/del') }}" class="btn btn-danger">Obriši</a>
+		
+		@endif
+		
 		</div>
     </div>
 </div>
--- routes/web.php
+++ routes/web.php
@@ -32,7 +32,7 @@ Route::get('/item/{id}/edit', 'ItemsController@edit');
 Route::post('/item/{id}/edit', 'ItemsController@update');
 
 //brisanje Item-a
-Route::delete('/item/{id}', 'ItemsController@destroy');
+Route::get('/item/{id}/del', 'ItemsController@destroy');
 
 
 
